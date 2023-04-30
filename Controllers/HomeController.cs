b00e37965ba13e327d27c54094e0503db83dd86d
Владимир Баженов <vova.bazhenov.77@inbox.ru>
2023-04-30 17:21:43 +0500

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using AniGeekAPI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AniGEEK.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Catalog()
+        {
+            var client = new AniGeekClient("SecretToken");
+            var products = await client.GetProductsAsync();
+            if (products is null) throw new Exception("No Products");
+            return View(products);
+        }
+
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}

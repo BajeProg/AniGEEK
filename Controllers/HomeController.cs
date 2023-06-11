c2f4189c64658e54c425d078aca9b84c4b1e6f29
Владимир Баженов <vova.bazhenov.77@inbox.ru>
2023-06-11 20:47:03 +0500

    Изменения 11.06.2023

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,32 +5,26 @@ namespace AniGEEK.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private AniGeekClient client = new AniGeekClient("SecretToken");
 
-        public async Task<IActionResult> Catalog()
-        {
-            var client = new AniGeekClient("SecretToken");
-            var products = await client.GetProductsAsync();
-            if (products is null) throw new Exception("No Products");
-            return View(products);
-        }
+        #region non_logic
+        public IActionResult Index() => View();
 
-        public IActionResult Main()
-        {
-            return View();
-        }
+        public IActionResult Main() => View();
 
-        public IActionResult Login()
-        {
-            return View();
-        }
+        public IActionResult Login() => View();
 
         public IActionResult Contact() => View();
 
         public IActionResult QnA() => View();
+        #endregion
+
+        public async Task<IActionResult> Catalog()
+        {
+            var products = await client.GetProductsAsync();
+            if (products is null) throw new Exception("No Products");
+            return View(products);
+        }
 
         public IActionResult Profile()
         {
@@ -38,6 +32,28 @@ namespace AniGEEK.Controllers
             if (HttpContext.Session.GetInt32("userid") == null)
                 return Redirect("~/Home/Login");
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Profile(string login, string pass)
+        {
+            var user = await client.LoginAsync(login, pass, HttpContext.Session.Id);
+            if (user is null) throw new Exception("Неправильный логин или пароль");
+            HttpContext.Session.SetInt32("userid", (int)user.ID);
+            return Redirect("~/Home/Profile");
+        }
+
+        public async Task<IActionResult> Item(int id)
+        {
+            var product = await client.GetProductAsync(id);
+            if (product is null) throw new Exception("No product");
+            return View(product);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("`/Home/Login");
         }
     }
 }

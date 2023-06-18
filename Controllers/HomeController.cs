d571d77fd16120d70e45e8b0367536a8267e16f6
Владимир Баженов <vova.bazhenov.77@inbox.ru>
2023-06-18 14:46:06 +0500

    Теперь можно делать и просматривать свои заказы, а также просматривать свой список избранного

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -55,14 +55,22 @@ namespace AniGEEK.Controllers
             return View(product);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Item(string id)
+        public async Task<IActionResult> Order(int id)
         {
-            var product = await client.GetProductAsync(int.Parse(id));
+            var product = await client.GetProductAsync(id);
             if (product is null) throw new Exception("No product");
-            if (Info.user is null) throw new Exception("No user");
-            await Info.user.AddToCartAsync(product);
-            return Redirect("~/Home/Cart");
+            if (Info.user is null) Redirect("~/Hpme/Login");
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Order(string address, int id)
+        {
+            var product = await client.GetProductAsync(id);
+            if (product is null) throw new Exception("No product");
+            if (Info.user is null) Redirect("~/Hpme/Login");
+            await Info.user!.MakeOrderAsync(product, address);
+            return Redirect("~/Home/Orders");
         }
 
         public IActionResult Logout()
@@ -76,6 +84,22 @@ namespace AniGEEK.Controllers
         {
             if(Info.user is null) return Redirect("~/Home/Login");
             var products = await Info.user.GetProductsFromCartAsync();
+            if (products is null) throw new Exception("No Products");
+            return View(products);
+        }
+
+        public async Task<IActionResult> Orders()
+        {
+            if (Info.user is null) return Redirect("~/Home/Login");
+            var orders = await Info.user.GetOrdersAsync();
+            if (orders is null) throw new Exception("No Products");
+            return View(orders);
+        }
+
+        public async Task<IActionResult> Favorites()
+        {
+            if (Info.user is null) return Redirect("~/Home/Login");
+            var products = await Info.user.GetProductsFromFavoriteAsync();
             if (products is null) throw new Exception("No Products");
             return View(products);
         }
